d68295f2fdd3dbf0c7f7d62fb61c09ac97b43617
Ron Lane <theartfuldodger2012@gmail.com>
2024-04-25 11:18:42 -0400

    Initial Commit

---
+++ MV/MVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVApp.swift
+//  MV
+//
+//  Created by Ron Lane on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct MVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
